6477e550f948a6ddf9512d20863f44fbde319011
SYSTEMS\zeeshan.alam <zeeshan.alam@systemsltd.com>
2023-01-16 15:17:28 +0500

    project scaffold using hardhat

---
+++ contracts/ExplorerToken.sol
@@ -0,0 +1,10 @@
+// contracts/ExplorerToken.sol
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ExplorerToken is ERC20 {
+    constructor() {}
+}
