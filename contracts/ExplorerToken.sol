5402f2ec63118743e38663d40e53c1157ecaf827
SYSTEMS\zeeshan.alam <zeeshan.alam@systemsltd.com>
2023-01-17 12:58:24 +0500

    mint tokens

--- contracts/ExplorerToken.sol
+++ contracts/ExplorerToken.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract ExplorerToken is ERC20Capped {
     address payable public owner;
@@ -16,6 +17,23 @@ contract ExplorerToken is ERC20Capped {
         owner = payable(msg.sender);
         _mint(owner, initialSupply * (10 ** decimals()));
     }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount * (10 ** decimals()));
+    }
+
+    // function _mint(
+    //     address account,
+    //     uint256 amount
+    // ) internal virtual override(ERC20Capped, ERC20) {
+    //     require(
+    //         ERC20.totalSupply() + (amount * (10 ** decimals())) <= cap(),
+    //         "ERC20Capped: cap exceeded"
+    //     );
+    //     super._mint(account, amount);
+    // }
+
+    // function burn(address account, uint256 amount) public virtual onlyOwner {}
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can call this function.");
